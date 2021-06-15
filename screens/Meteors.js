a5255208990aadbe74c1ffa8aa6d3bfa7bfa89bb
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-15 20:11:03 +0530

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert } from 'react-native';
 import axios from "axios";
 
 export default class MeteorScreen extends Component {
